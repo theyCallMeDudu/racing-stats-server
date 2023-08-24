7f96cfdd70568cf326dc4af92144ebab1d01f9fe
Eduardo Coelho <coelho.edu96@gmail.com>
2023-08-24 19:44:22 -0300

    Instalando cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const routerPilotos = require('./routes/F1/piloto')
 const app = express()
 const port = 8000
 
+app.use(cors())
 app.use('/f1', routerPilotos)
 
 app.listen(port, () => {
